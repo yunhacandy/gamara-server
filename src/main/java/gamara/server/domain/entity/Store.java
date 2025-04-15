7d295ccefc4ffc58680c7488f09c5245e970d6c9
yunhacandy <yunhacandy@gmail.com>
2025-04-15 15:16:45 +0900

    feat: 가게 관련 엔티티 추가

---
+++ src/main/java/gamara/server/domain/entity/Store.java
@@ -0,0 +1,41 @@
+package gamara.server.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "store")
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    private double latitude;
+
+    private double longitude;
+
+    private int recommendCount;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
