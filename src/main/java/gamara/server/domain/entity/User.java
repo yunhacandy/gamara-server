d461e3a10f2da7ba62189ba60a3d966133407265
yunhacandy <yunhacandy@gmail.com>
2025-04-17 22:23:14 +0900

    refactor: validation 관련 어노테이션 제거

--- src/main/java/gamara/server/domain/entity/User.java
+++ src/main/java/gamara/server/domain/entity/User.java
@@ -10,8 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,12 +28,10 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
-    @Email
     @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
-    @Pattern(regexp = "[가-힣a-zA-Z0-9]{3,10}", message = "닉네임은 한글, 영어, 숫자를 조합해 3글자 이상, 10글자 이하입니다.")
     private String nickname;
 
     @Column(nullable = false)
