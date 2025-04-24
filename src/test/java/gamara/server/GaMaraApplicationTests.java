cd83aabdbe2fd1d0b31eac3579cd9d0ee4b18b96
yunhacandy <yunhacandy@gmail.com>
2025-04-24 15:12:41 +0900

    fix: SpringBootTest가 정상적으로 돌아가도록 프로필 파일을 test 로 설정

--- src/test/java/gamara/server/GaMaraApplicationTests.java
+++ src/test/java/gamara/server/GaMaraApplicationTests.java
@@ -1,13 +1,16 @@
 package gamara.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class GaMaraApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
