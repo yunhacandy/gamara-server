e61a34eea71f6aa1d35c96057a89b116fd8d9671
yunhacandy <yunhacandy@gmail.com>
2025-04-28 23:08:55 +0900

    refactor: 인증 추가

--- src/main/java/gamara/server/controller/ReviewController.java
+++ src/main/java/gamara/server/controller/ReviewController.java
@@ -29,7 +29,7 @@ public class ReviewController {
     @Operation(summary = "후기 등록", description = "맵기 단계, 땅콩 소스 정도, 사골 유무, 얼얼함 단계 등 다양한 정보를 담은 후기를 등록합니다")
     @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping
-    public Response<?> registerReview(@Valid @ModelAttribute ReviewCreateRequest request, long userId)
+    public Response<?> registerReview(@Valid @ModelAttribute ReviewCreateRequest request, @AuthenticationPrincipal long userId)
             throws ImageException {
         reviewService.registerReview(request, userId);
         return Response.createSuccessWithNoData("[Review Controller] Register Review");
