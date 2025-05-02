661ac3e2a9928a69fc5b7596e8a2c829f4ff7c77
yunhacandy <yunhacandy@gmail.com>
2025-05-02 13:32:49 +0900

    refactor: securityConfig 수정

--- src/main/java/gamara/server/config/SecurityConfig.java
+++ src/main/java/gamara/server/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,14 +16,16 @@ public class SecurityConfig {
 
     private static final String[] WHITE_LIST = {
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/auth/**"
     };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers(WHITE_LIST).permitAll()
                         .anyRequest().authenticated()
                 );
