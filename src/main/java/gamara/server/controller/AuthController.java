c9945694e20b08cab317503f774bcc045f9b7c0c
yunhacandy <yunhacandy@gmail.com>
2025-05-08 15:52:04 +0900

    feat: 카카오 로그인 swagger 설명 추가

--- src/main/java/gamara/server/controller/AuthController.java
+++ src/main/java/gamara/server/controller/AuthController.java
@@ -4,6 +4,7 @@ import gamara.server.common.Response;
 import gamara.server.dto.request.KakaoLoginRequest;
 import gamara.server.dto.LoginResultDto;
 import gamara.server.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "카카오 로그인", description = "카카오에서 받은 액세스 토큰을 통해 회원가입 또는 로그인하는 API")
     @PostMapping("/kakao/login")
     public Response<LoginResultDto> loginKakao(@RequestBody KakaoLoginRequest request) {
         LoginResultDto response = authService.loginKakao(request);
