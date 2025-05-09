00f99a95b6ebf81019436b8eb55c740e9ce8d801
yunhacandy <yunhacandy@gmail.com>
2025-05-09 13:57:09 +0900

    feat: auth 관련 기능에도 공통응답을 따르고 있음을 명시

--- src/main/java/gamara/server/controller/AuthController.java
+++ src/main/java/gamara/server/controller/AuthController.java
@@ -1,12 +1,15 @@
 package gamara.server.controller;
 
 import gamara.server.common.Response;
-import gamara.server.dto.LoginResultDto;
-import gamara.server.dto.ReissueResultDto;
 import gamara.server.dto.request.KakaoLoginRequest;
+import gamara.server.dto.LoginResultDto;
 import gamara.server.dto.request.ReissueRequest;
+import gamara.server.dto.ReissueResultDto;
 import gamara.server.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +28,7 @@ public class AuthController {
     private final AuthService authService;
 
     @Operation(summary = "카카오 로그인", description = "카카오에서 받은 액세스 토큰을 통해 회원가입 또는 로그인하는 API")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping("/kakao/login")
     public Response<LoginResultDto> loginKakao(@RequestBody KakaoLoginRequest request) {
         LoginResultDto response = authService.loginKakao(request);
@@ -32,6 +36,7 @@ public class AuthController {
     }
 
     @Operation(summary = "access token 재발급", description = "만료된 access token 을 refresh token으로 재발급 하는 API")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping("/reissue")
     public Response<ReissueResultDto> reissueToken(@RequestBody ReissueRequest reissueRequest) {
         return Response.createSuccess(authService.reissueToken(reissueRequest.refreshToken()));
