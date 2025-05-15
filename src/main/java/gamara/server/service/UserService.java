8bedd0c5534070cf37f0c916a041a93d4127a55b
yunhacandy <yunhacandy@gmail.com>
2025-05-15 14:46:22 +0900

    refactor: user 조회 한번만 하도록 수정

--- src/main/java/gamara/server/service/UserService.java
+++ src/main/java/gamara/server/service/UserService.java
@@ -6,7 +6,6 @@ import gamara.server.converter.UserConverter;
 import gamara.server.domain.dto.UserInfoDto;
 import gamara.server.domain.entity.User;
 import gamara.server.repository.UserRepository;
-import gamara.server.validator.EntityValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,14 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
-    private final EntityValidator entityValidator;
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public UserInfoDto getUserInfo(long userId) {
-        entityValidator.validateUserIsActive(userId);
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        if (user.isDeleted()) {
+            throw new AppException(ErrorCode.USER_ALREADY_DELETED);
+        }
         return UserConverter.toUserInfoDto(user);
     }
 }
