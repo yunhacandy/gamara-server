2f2b43fafe211c683562419b3a8d5f0512efbd04
yunhacandy <yunhacandy@gmail.com>
2025-05-22 13:34:38 +0900

    style: 코드 컨벤션에 맞춰서 공백 삭제

--- src/main/java/gamara/server/repository/RefreshTokenRepository.java
+++ src/main/java/gamara/server/repository/RefreshTokenRepository.java
@@ -5,6 +5,5 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
-
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
