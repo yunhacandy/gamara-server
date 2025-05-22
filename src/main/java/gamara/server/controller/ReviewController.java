39f622eedfd57f2110262b5b64cf46b95dd4535f
yunhacandy <yunhacandy@gmail.com>
2025-05-22 14:51:10 +0900

    refactor: 사용하지 않는 import statement 삭제

--- src/main/java/gamara/server/controller/ReviewController.java
+++ src/main/java/gamara/server/controller/ReviewController.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
