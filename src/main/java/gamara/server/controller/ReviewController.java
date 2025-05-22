7316b29b19c41bf3c454695533b230dc887f7bad
yunhacandy <yunhacandy@gmail.com>
2025-05-22 14:48:06 +0900

    refactor: 후기 관련 api 응답 수정

--- src/main/java/gamara/server/controller/ReviewController.java
+++ src/main/java/gamara/server/controller/ReviewController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,22 +34,24 @@ public class ReviewController {
     @Operation(summary = "후기 등록", description = "맵기 단계, 땅콩 소스 정도, 사골 유무, 얼얼함 단계 등 다양한 정보를 담은 후기를 등록합니다")
     @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @PostMapping
-    public Response<?> registerReview(@Valid @ModelAttribute ReviewCreateRequest request,
+    public Response<Void> registerReview(@Valid @ModelAttribute ReviewCreateRequest request,
                                       @AuthenticationPrincipal AuthDetails authDetails)
             throws ImageException {
         long userId = Long.parseLong(authDetails.getUserId());
         reviewService.registerReview(request, userId);
-        return Response.createSuccessWithMessage("[Review Controller] Register Review");
+        log.trace("[Review Controller] Register Review");
+        return Response.createSuccessWithNoData();
     }
 
     @Operation(summary = "후기 삭제", description = "회원은 본인이 작성한 후기를 삭제할 수 있다")
     @ApiResponse(content = @Content(schema = @Schema(implementation = Response.class)))
     @DeleteMapping("/{reviewId}")
-    public Response<?> deleteReview(@PathVariable("reviewId") long reviewId,
+    public Response<Void> deleteReview(@PathVariable("reviewId") long reviewId,
                                     @AuthenticationPrincipal AuthDetails authDetails)
             throws ImageException {
         long userId = Long.parseLong(authDetails.getUserId());
         reviewService.deleteReview(reviewId, userId);
-        return Response.createSuccessWithMessage("[Review Controller] Delete Review");
+        log.trace("[Review Controller] Delete Review");
+        return Response.createSuccessWithNoData();
     }
 }
